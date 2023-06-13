562abbe3f4e4178adb2e67590f24a43e8aef7e09
Renzhijie110 <97765487+Renzhijie110@users.noreply.github.com>
2023-06-13 15:31:33 -0400

    modified:   main.py 	new file:   speech_recognition/__init__.py 	new file:   speech_recognition/__main__.py 	new file:   speech_recognition/audio.py 	new file:   speech_recognition/exceptions.py 	new file:   speech_recognition/flac-linux-x86 	new file:   speech_recognition/flac-linux-x86_64 	new file:   speech_recognition/flac-mac 	new file:   speech_recognition/flac-win32.exe 	new file:   speech_recognition/pocketsphinx-data/en-US/LICENSE.txt 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/README 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/feat.params 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/mdef 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/means 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/noisedict 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/sendump 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/transition_matrices 	new file:   speech_recognition/pocketsphinx-data/en-US/acoustic-model/variances 	new file:   speech_recognition/pocketsphinx-data/en-US/language-model.lm.bin 	new file:   speech_recognition/pocketsphinx-data/en-US/pronounciation-dictionary.dict 	new file:   speech_recognition/recognizers/__init__.py 	new file:   speech_recognition/recognizers/whisper.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import openai
 import SpeechRecognition as sr
 
-openai.api_key = 'YOUR_API_KEY'
+openai.api_key = 'API_KEY'
 
 # Set up OpenAI API credentials
 def get_chatbot_response(input_text):
     response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=input_text,
-        max_tokens=100
+        model='gpt-3.5-turbo-0301',
+        message=input_text,
+        temperature = 0.0
     )
     return response.choices[0].text.strip()
 
